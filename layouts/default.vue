240492c46516857e63c196ee2880eaa4dfb4de84
Alex Guzey <olexandr.guzey@publicfinance.gov.ua>
2019-10-08 19:04:28 +0300

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,14 @@
 <template>
-  <v-app class="white" id="app-container">
-    <a-header
-      v-model="drawer"
-    ></a-header>
-    <a-side-bar
-      v-if="$breakpoint.is('smAndDown')"
-      v-model="drawer"
-    ></a-side-bar>
-    <v-content class="default-layout__main-content pb-0 grey lighten-2">
-      <v-layout wrap px-0 pt-0>
-        <nuxt />
-      </v-layout>
-    </v-content>
-    <a-footer></a-footer>
-    <!--<pop-up
-      action-type="Actions"
-      type="ErrorData"
-      :dialog-data="dialogData"
-      v-model="dialogShow"
-      @close="dialogShow = false"
-    ></pop-up>-->
-  </v-app>
+  <!--<nuxt />-->
 </template>
 
 <script>
-import AFooter from '~/components/default-layout/AFooter'
-// import AEntrance from '~/components/default-layout/AEntrance'
-import ASideBar from '~/components/default-layout/ASideBar'
-import AHeader from '~/components/default-layout/AHeader'
-// import PopUp from '~/components/shared/popup/PopUp'
 export default {
-  components: {
-    AHeader,
-    // PopUp,
-    ASideBar,
-    // AEntrance,
-    AFooter
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      miniVariant: false,
-    }
-  }
 }
 </script>
 
 <style lang="scss">
   .default-layout{
-    &__container {
-      /*background-color: rgba(255,255,255, .8);*/
-      min-height: 1500px;
-    }
+
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,35 @@
 <template>
-  <section class="home-page">
-    <v-parallax
-      class="home-page__paralax px-0"
-      src="img/home/paralax.jpg"
-      height="800"
-    >
-      <v-layout
-        class="home-page__subtitle-layout"
-        justify-center fill-height align-center my-5
-      >
-        <v-layout class="home-page__subtitle-wrapper" wrap justify-center fill-height align-center>
-          <v-flex xs12 class="text-xs-center">
-            <h3 class="home-page__subtitle primary--text">
-              Сайт інвесторів житлового комплексу "ТРИ БОГАТИРІ"
-            </h3>
-            <p class="grey--text lighten-2 title">
-              м.Київ, вул Борщагівська, 182 В
-            </p>
-          </v-flex>
-        </v-layout>
-      </v-layout>
-
-      <v-container
-        grid-list-xl
-        py-0 my-5
-      >
-        <home-cards-menu></home-cards-menu>
-      </v-container>
-    </v-parallax>
-
-    <!--<v-parallax
-      class="home-page__paralax"
-      dark
-      src="img/home/paralax.jpg"
-      height="150"
-    ></v-parallax>-->
-  </section>
+  <div></div>
 </template>
 
 <script>
-import HomeCardsMenu from '~/components/home/HomeCardsMenu.vue'
+// if (process.browser) {
+//   // require('https://identity.netlify.com/v1/netlify-identity-widget.js')
+//   // require('https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js')
+// }
 export default {
-  name: 'HomeCard',
-  components: {
-    HomeCardsMenu
-  },
+  name: 'Home',
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+        // { src: 'https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js' }
+      ]
     };
   },
   data() {
     return {
       test: 'test2'
     }
+  },
+  mounted() {
+    // require('https://identity.netlify.com/v1/netlify-identity-widget.js')
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .home-page {
-    width: 100%;
-    &__paralax{
-      width: 100%;
-    }
-    &__paralax-title{
-      padding: 10px 30px;
-      background-color: rgba(0,0,0,.5);
-      border-radius: 20px;
-      font-size: 60px;
-      text-shadow: 2px 2px 4px black;
-    }
-    &__subtitle{
-      font-size: 2rem;
-    }
-    &__subtitle-layout{
-      margin: 0 -14px;
-    }
-    &__subtitle-wrapper{
-      background-color: rgba(255,255,255,.8);
-      height: 160px;
-    }
-    &__card{
-      min-width: 250px;
-      max-width: 300px;
-    }
+
   }
 </style>
