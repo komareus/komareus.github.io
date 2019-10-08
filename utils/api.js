9a5046d61aefcb65fc189536a15e149759a39040
Alex Guzey <olexandr.guzey@publicfinance.gov.ua>
2019-10-08 17:54:13 +0300

    switch brunch

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const contentBranch = 'prod';
+const contentBranch = 'master';
 const repositoryName = 'komareus';
 const listServerItems = `https://api.github.com/repos/${repositoryName}/${repositoryName}.github.io/contents/content`;
 const serverContentItems = `https://raw.githubusercontent.com/${repositoryName}/${repositoryName}.github.io/${contentBranch}/content`;
