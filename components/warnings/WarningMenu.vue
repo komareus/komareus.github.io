440379c348f806d29306446dc60b44db0abfe65d
Alex Guzey <olexandr.guzey@publicfinance.gov.ua>
2019-10-09 15:03:07 +0300

    add warn button

---
+++ components/warnings/WarningMenu.vue
@@ -0,0 +1,62 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div class="warning-menu">
+    <v-menu offset-y left class="warning-menu__menu">
+      <button slot="activator" class="warning-menu__button error">
+        Важливі повідомлення
+      </button>
+      <v-list class="warning-menu__list primary--text pt-0">
+        <v-list-tile class="warning-menu__title" wrap>
+          <h4>Список оголошень</h4>
+        </v-list-tile>
+
+        <v-list-tile
+          class="compare-header-button__list-tile"
+          v-for="(item, index) in list"
+          :key="index"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title class="compare-header-button__list-tile-title">
+              {{ item.title }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WarningMenu',
+  props: {
+    list: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .warning-menu{
+    width: 100%;
+    &__menu{
+      width: 100%;
+    }
+    &__button{
+      height: 28px;
+      width: 100%;
+      border-radius: 0 0 12px 12px;
+      text-transform: uppercase;
+    }
+    &__list-tile{
+      max-width: 400px;
+    }
+    &__list-tile-title{
+      font-size: 0.8rem!important;
+    }
+    &__title{
+      background-color: $c--opacity-blue;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,42 @@
       height="800"
     >
       <v-layout
+        class="home-page__label"
+        :mt-5="$breakpoint.is('xsOnly')"
+        justify-end
+        :justify-center="$breakpoint.is('xsOnly')"
+      >
+        <v-flex class="home-page__label-wrapper">
+          <v-layout justify-center align-center fill-height px-3 pb-3 pt-4 wrap>
+            <h3 class="home-page__label-title primary--text text-xs-center">
+              Сайт інвесторів житлового комплексу <br> "ТРИ БОГАТИРІ"
+            </h3>
+            <p class="grey--text lighten-2 ma-0">
+              м.Київ, вул Борщагівська, 182 В
+            </p>
+          </v-layout>
+          <v-layout>
+            <warning-menu
+              class="home-page__warning"
+            ></warning-menu>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+      <!--<v-layout
         class="home-page__subtitle-layout"
         justify-center fill-height align-center my-5
       >
         <v-layout class="home-page__subtitle-wrapper" wrap justify-center fill-height align-center>
           <v-flex xs12 class="text-xs-center">
-            <h3 class="home-page__subtitle primary--text">
+            <h3 class="home-page__subtitle primary&#45;&#45;text">
               Сайт інвесторів житлового комплексу "ТРИ БОГАТИРІ"
             </h3>
-            <p class="grey--text lighten-2 title">
+            <p class="grey&#45;&#45;text lighten-2 title">
               м.Київ, вул Борщагівська, 182 В
             </p>
           </v-flex>
         </v-layout>
-      </v-layout>
+      </v-layout>-->
 
       <v-container
         grid-list-xl
@@ -40,9 +62,11 @@
 
 <script>
 import HomeCardsMenu from '~/components/home/HomeCardsMenu.vue'
+import WarningMenu from '~/components/warnings/WarningMenu'
 export default {
   name: 'HomeCard',
   components: {
+    WarningMenu,
     HomeCardsMenu
   },
   head() {
@@ -84,6 +108,19 @@ export default {
     &__card{
       min-width: 250px;
       max-width: 300px;
+    }
+    &__label{
+      margin-top: -6px;
+    }
+    &__label-wrapper{
+      background-color: rgba(255,255,255,.7);
+      min-height: 90px;
+      max-width: 450px;
+      border-radius: 12px;
+    }
+    &__warning{
+      margin-top: -8px;
+      width: 100%;
     }
   }
 </style>
